c6901e3c52012bcae025842935f6d71f9a138cc0
cookeem <cookeem@qq.com>
2024-10-29 14:35:01 +0800

    v1.6.6

--- pkg/install_scripts/kubernetes/harbor_update_docker_certs.sh
+++ pkg/install_scripts/kubernetes/harbor_update_docker_certs.sh
@@ -2,7 +2,7 @@
 export CERT_PATH="{{ $certPath }}/certs.d"
 rm -rf ${CERT_PATH}/{{ $.imageRepoDomainName }}
 mkdir -p ${CERT_PATH}/{{ $.imageRepoDomainName }}
-export INGRESS_SECRET_NAME=$(kubectl -n {{ $.dory.imageRepo.internal.namespace }} get secrets | grep "harbor-ingress" | awk '{print $1}')
+export INGRESS_SECRET_NAME=$(kubectl -n {{ $.dory.imageRepo.internal.namespace }} get secrets | grep "ingress" | awk '{print $1}')
 kubectl -n {{ $.dory.imageRepo.internal.namespace }} get secrets ${INGRESS_SECRET_NAME} -o jsonpath='{ .data.ca\.crt }'  | base64 -d > ${CERT_PATH}/{{ $.imageRepoDomainName }}/ca.crt
 kubectl -n {{ $.dory.imageRepo.internal.namespace }} get secrets ${INGRESS_SECRET_NAME} -o jsonpath='{ .data.tls\.crt }' | base64 -d > ${CERT_PATH}/{{ $.imageRepoDomainName }}/{{ $.imageRepoDomainName }}.cert
 kubectl -n {{ $.dory.imageRepo.internal.namespace }} get secrets ${INGRESS_SECRET_NAME} -o jsonpath='{ .data.tls\.key }' | base64 -d > ${CERT_PATH}/{{ $.imageRepoDomainName }}/{{ $.imageRepoDomainName }}.key
